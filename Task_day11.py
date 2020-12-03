afbf1cc035ebad39dcd6049cd485da5c5e63eada
D-deva3 <72125640+D-deva3@users.noreply.github.com>
2020-12-03 20:22:10 +0530

    Create Task_day11.py

---
+++ Task_day11.py
@@ -0,0 +1,22 @@
+#1) program using zip() function and list() function, create a merged list of tuples from the two lists given.
+list_1=[1,2,3,4,5]
+list_2=[6,7,8,9,10]
+x=zip(list_1,list_2)
+print(tuple(x))
+
+#2) create a range from 1 to 8. Then using zip, merge the given list and the range together to create a new list of tuples.
+
+num=range(1,8)
+list_3=[12,22,32,42,52,62,72,82]
+y=zip(num,list_3)
+print(tuple(y))
+
+#3) Using sorted() function, sort the list in ascending order.
+list_4=[78,90,678,3,7,34,34,1567]
+result=sorted(list_4) #if we want in descending order use ,reverse=True
+print(result)
+
+#4) program using filter function, filter the even numbers so that only odd numbers are passed to the new list.
+list_5=[1,9,6,8,23,5,6,12,19,13,15,4]
+new_list=list(filter(lambda x:(x%2!=0),list_5))
+print(new_list)
